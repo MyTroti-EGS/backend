9070b1e4171bfdaa95cee3d94d111280627df31d
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2025-03-19 09:25:43 +0000

    Get scooters from GraphQL
    Signed-off-by: Francisco Ribeiro <francisco.ribeiro@ua.pt>

--- src/graphql/v1/ScooterInfoGQL.ts
+++ src/graphql/v1/ScooterInfoGQL.ts
@@ -1,23 +1,35 @@
-import { GraphQLEnumType, GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLEnumType, GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
 import { LocationGQL } from './common/types/LocationGQL';
 
 const SCOOTER_MONITOR_URL = process.env.SCOOTER_MONITOR_URL;
 const SCOOTER_MONITOR_API_KEY = process.env.SCOOTER_MONITOR_API_KEY;
 
 enum ScooterStatus {
-    AVAILABLE = 'available',
-    OCCUPIED = 'occupied',
-    MAINTENANCE = 'maintenance',
-    CHARGING = 'charging',
+    AVAILABLE = 'AVAILABLE',
+    OCCUPIED = 'OCCUPIED',
+    MAINTENANCE = 'MAINTENANCE',
+    CHARGING = 'CHARGING',
+}
+
+type ScooterInfo = {
+    id: number,
+    latitude: number,
+    longitude: number,
+    battery_percentage: string,
+    mac_address: string,
+    serial_number: string,
+    status: ScooterStatus,
 }
 
 export class ScooterInfoGQL {
     public static scooterInfoType = new GraphQLObjectType({
         name: 'ScooterInfo',
         fields: {
-            id: { type: GraphQLString },
+            id: { type: new GraphQLNonNull(GraphQLString) },
             location: { type: LocationGQL.locationType },
-            battery: { type: GraphQLString },
+            battery: { type: new GraphQLNonNull(GraphQLString) },
+            mac_address: { type: new GraphQLNonNull(GraphQLString) },
+            serial_number: { type: new GraphQLNonNull(GraphQLString) },
             status: { 
                 type: new GraphQLEnumType({
                     name: 'ScooterStatus',
@@ -44,15 +56,27 @@ export class ScooterInfoGQL {
             headers: {
                 "x-api-key": SCOOTER_MONITOR_API_KEY,
             },
+        }).catch((error) => {
+            throw new Error(`Failed to fetch scooter info: Server is down`);
         });
 
         if (!response.ok) {
             throw new Error(`Failed to fetch scooter info: ${response.statusText}`);
         }
 
-        const json = await response.json();
+        const json = id ? 
+            [ await response.json() ]:
+            await response.json();
 
-        if (id) return [ json ];
-        return json;
+        return json.map((scooter: ScooterInfo) => {
+            return {
+                id: scooter.id,
+                location: { latitude: scooter.latitude, longitude: scooter.longitude },
+                battery: scooter.battery_percentage,
+                mac_address: scooter.mac_address,
+                serial_number: scooter.serial_number,
+                status: scooter.status,
+            };
+        });
     }
 }
